61895965c5dc35fe81d4b7e894d48b5d8c65e6a9
ajaypareek <62881091+akayjoshi@users.noreply.github.com>
2021-04-24 11:36:18 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+-&_4l_$j%5h72_m1424t86i5&ts7a3bbo#h_dll0^&oqffqkl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
